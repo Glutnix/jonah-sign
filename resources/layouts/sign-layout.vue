10ebf741e44423101adfc8682ea34af6de258d6f
Brett Taylor <brett@webfroot.co.nz>
2017-03-31 21:59:55 +1300

    Add basic prototype page for signage

---
+++ resources/layouts/sign-layout.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="Sign">
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+@import '~assets/css/variables';
+
+html, body {
+  height: 100%;
+}
+
+body {
+  background-color: $black;
+  color: $white;
+  font-size: 80px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h1, h2, h3, h4, h5, h6, p {
+  margin: 3vw 0;
+  font-weight: normal;
+}
+
+small {
+  font-size: 0.6em;
+}
+
+.Sign {
+  flex: 0 1 auto;
+  text-align: center;
+  padding: 0.5em;
+}
+
+</style>
---
+++ resources/pages/sign.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <img src="~assets/img/jonah-whale.svg" alt="" class="Whale" />
+    <h1>Jonah Sign</h1>
+    <hr />
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h4>
+    <h5>Heading 5</h5>
+    <h6>Heading 6</h6>
+    <p>
+      Some regular text that wraps, complete with 😇 👼 emoji
+    </p>
+    <p>
+      <small>Some smaller text</small>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'sign-layout'
+}
+</script>
+
+<style lang="scss">
+@import '~assets/css/variables';
+
+.Whale {
+  width: 50vw;
+  height: auto;
+}
+
+</style>
