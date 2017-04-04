0475ad53ed063a25af5ada0a93932f2bf798259e
Brett Taylor <brett@webfroot.co.nz>
2017-04-04 20:57:54 +1200

    Move unused footer out of main css and into footer

--- resources/components/Footer.vue
+++ resources/components/Footer.vue
@@ -3,3 +3,13 @@
     <a href="https://github.com/Glutnix/jonah-sign">GitHub</a>
   </footer>
 </template>
+
+<style lang="scss">
+@import '~assets/css/variables';
+
+footer {
+  padding: 20px;
+  text-align: center;
+  border-top: 1px solid $lightgray;
+}
+</style>
